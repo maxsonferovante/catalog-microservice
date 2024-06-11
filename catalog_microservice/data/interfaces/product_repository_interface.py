b2d66c430e8d7186012ea9d69a28ba4eb80445c8
Maxson Almeida <maxsonferovante@gmail.com>
2024-06-10 22:46:10 -0300

    feat: Add ProductRepositoryInterface for inserting and selecting products

---
+++ catalog_microservice/data/interfaces/product_repository_interface.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+from catalog_microservice.domain.models.product import Products
+
+class ProductRepositoryInterface(ABC):
+    @abstractmethod
+    def insert_product(self, name: str, description: str, price: float, category_id: int) -> None:
+        raise NotImplementedError
+    
+    @abstractmethod
+    def select_product(self, product_id: int) -> List[Product]:
+        raise NotImplementedError    
