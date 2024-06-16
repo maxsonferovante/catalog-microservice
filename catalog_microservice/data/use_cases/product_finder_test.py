abca67c7bcceba36c0c1f5b19e20da304674d599
Maxson Almeida <maxsonferovante@gmail.com>
2024-06-16 16:35:28 -0300

    feat: Add unit tests for ProductFinder class

---
+++ catalog_microservice/data/use_cases/product_finder_test.py
@@ -0,0 +1,58 @@
+from catalog_microservice.infra.database.tests.products_repository import ProductRepositorySpy
+from catalog_microservice.data.use_cases.product_finder import ProductFinder
+
+def test_find_with_product_not_found():
+    product_repository = ProductRepositorySpy()
+    
+    product_finder = ProductFinder(product_repository)
+    
+    try:
+        product_finder.find(2)
+    except Exception as e:
+        assert str(e) == 'Product not found'    
+        
+def test_find_with_invalid_product_repository():
+    try:
+        ProductFinder(None)
+    except Exception as e:
+        assert str(e) == 'Product repository is required'
+        
+def test_find():
+    product_repository = ProductRepositorySpy()
+    
+    product_finder = ProductFinder(product_repository)
+    
+    response = product_finder.find(1)
+    
+    assert product_repository.select_product_atributes['product_id'] == 1
+    
+    assert response['type'] == 'Products'
+    assert response['count'] == 1
+    assert response['atributes'] is not None
+    assert response['atributes'][0].id == 1
+    assert response['atributes'][0].name == 'Product 1'
+    assert response['atributes'][0].description == 'Description 1'
+    assert response['atributes'][0].price == 10.0
+    assert response['atributes'][0].category_id == 1
+    
+
+def test_find_with_invalid_product_id():
+    product_repository = ProductRepositorySpy()
+    
+    product_finder = ProductFinder(product_repository)
+    
+    try:
+        product_finder.find(None)
+    except Exception as e:
+        assert str(e) == 'Product ID must be a number'
+        
+    try:
+        product_finder.find('1')
+    except Exception as e:
+        assert str(e) == 'Product ID must be a number'
+        
+    try:
+        product_finder.find(-1)
+    except Exception as e:
+        assert str(e) == 'Product ID must be a positive number'
+        
