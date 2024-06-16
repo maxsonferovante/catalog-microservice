45852a4e73730646e99267c2be8f4eb8479f9a9d
Maxson Almeida <maxsonferovante@gmail.com>
2024-06-16 14:49:35 -0300

    feat: Update Category and Product entities with autoincrement and default timestamps

--- catalog_microservice/infra/database/entities/category.py
+++ catalog_microservice/infra/database/entities/category.py
@@ -1,14 +1,15 @@
+from datetime import datetime
+
 from sqlalchemy import Column, Integer, String, DateTime
 from catalog_microservice.infra.database.settings.base import Base
 
-
 class Category(Base):
     __tablename__ = 'categories'
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True,autoincrement=True, index=True)
     name = Column(String, index=True)
     description = Column(String)
-    created_at = Column(DateTime, current_timestamp=True)
-    updated_at = Column(DateTime, current_timestamp=True)
+    created_at = Column(DateTime, autoincrement=True, default=datetime.now)
+    updated_at = Column(DateTime, autoincrement=True, default=datetime.now, onupdate=datetime.now)
     
     def __repr__(self):
         return f'Category(id={self.id}, name={self.name}, description={self.description})'
--- catalog_microservice/infra/database/entities/product.py
+++ catalog_microservice/infra/database/entities/product.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from sqlalchemy import Column, Integer, DateTime, Numeric, String
 
 from catalog_microservice.infra.database.settings.base import Base
@@ -5,16 +7,17 @@ from sqlalchemy.sql.schema import ForeignKey
 from sqlalchemy.orm import relationship
 from catalog_microservice.infra.database.entities.category import Category
 
+
 class Product(Base):
     __tablename__ = 'products'
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
     name = Column(String, index=True)
     description = Column(String)
     price = Column(Numeric(10, 2))
     category_id = Column(Integer, ForeignKey('categories.id'))
-    created_at = Column(DateTime, current_timestamp=True)
-    updated_at = Column(DateTime, current_timestamp=True)
+    created_at = Column(DateTime, autoincrement=True, default=datetime.now)
+    updated_at = Column(DateTime, autoincrement=True, default=datetime.now, onupdate=datetime.now)
     
     def __repr__(self):
         return f'Product(id={self.id}, name={self.name}, description={self.description}'
