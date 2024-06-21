51897f151e21c59a30f68303fd41c412df0b3d1b
Maxson Almeida <maxsonferovante@gmail.com>
2024-06-21 01:18:37 -0300

    refactor: Add CORS support to Flask app

--- catalog_microservice/main/server/server.py
+++ catalog_microservice/main/server/server.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from flask_cors import CORS
 
 from catalog_microservice.main.routes.product_routes import product_route_bp
 from catalog_microservice.main.routes.routes import home_bp
 
 app = Flask(__name__)
+
+
+CORS(app)
+
 
 app.register_blueprint(home_bp)
 app.register_blueprint(product_route_bp)
