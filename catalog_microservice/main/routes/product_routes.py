7f965b145c7dc6f556a91878e79bae06c4592633
Maxson Almeida <maxsonferovante@gmail.com>
2024-06-21 01:00:32 -0300

    feat: Add error handling for HTTP requests in product_routes.py

--- catalog_microservice/main/routes/product_routes.py
+++ catalog_microservice/main/routes/product_routes.py
@@ -5,6 +5,7 @@ from catalog_microservice.main.adapters.request_adapter import request_adapter
 from catalog_microservice.main.composers.product_finder_composer import product_finder_composer
 from catalog_microservice.main.composers.product_register_composer import product_register_composer
 
+from catalog_microservice.errors.error_handler import handle_errors
 
 product_route_bp = Blueprint('product_routes', __name__)
 
@@ -12,17 +13,22 @@ product_route_bp = Blueprint('product_routes', __name__)
 
 @product_route_bp.route('/products/finder', methods=['GET'])
 def finder_product():
+    http_response = None
     
-    http_response = request_adapter(request=request, 
-                                    controller=product_finder_composer()
-                                    )
+    try:
+        http_response = request_adapter(request=request, controller=product_finder_composer())
+    except Exception as error:
+        http_response = handle_errors(error)
+    
     return jsonify(http_response.body), http_response.status_code
 
 @product_route_bp.route('/products/register', methods=['POST'])
 def register_product():
     
-    http_response = request_adapter(request=request, 
-                                    controller=product_register_composer()
-                                    )
+    http_response = None
+    try:
+        http_response = request_adapter(request=request, controller=product_register_composer())
+    except Exception as error:
+        http_response = handle_errors(error)
     
     return jsonify(http_response.body), http_response.status_code
