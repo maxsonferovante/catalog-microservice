37cf482dd4d2fc4eb61d79bb8f08e7d746db5218
Maxson Almeida <maxsonferovante@gmail.com>
2024-06-21 00:58:40 -0300

    feat: Add error handling for HTTP requests in error_handler.py

---
+++ catalog_microservice/errors/error_handler.py
@@ -0,0 +1,31 @@
+from http import HTTPStatus
+
+from catalog_microservice.presentation.http_types import HttpResponse
+
+from catalog_microservice.errors.types.http_bad_request import HttpBadRequestError
+from catalog_microservice.errors.types.http_unprocessable_content import HttpUnprocessableContentError
+from catalog_microservice.errors.types.http_not_found import HttpNotFoundError
+
+def handle_errors(error: Exception) -> HttpResponse:
+    if isinstance(error, HttpBadRequestError, HttpNotFoundError, HttpUnprocessableContentError):
+        
+        return HttpResponse(status_code=error.status_code, body= {
+            'errors': [
+                {
+                    'title':error.name,
+                    'detail': error.message
+                }
+            ]
+            
+        })
+    
+    return HttpResponse(status_code=HTTPStatus.INTERNAL_SERVER_ERROR, body= {
+            'errors': [
+                {
+                    'title': 'Server Error',
+                    'detail': str(error)
+                }
+            ]
+            
+        })
+    
