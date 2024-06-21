3d80f7bc570adcc6a25c0ee22383fcd9fc081fee
Maxson Almeida <maxsonferovante@gmail.com>
2024-06-21 00:47:43 -0300

    feat: Add validation for product ID in product finder

--- catalog_microservice/data/use_cases/product_finder.py
+++ catalog_microservice/data/use_cases/product_finder.py
@@ -4,6 +4,11 @@ from catalog_microservice.domain.use_cases.product_finder_interface import Produ
 from catalog_microservice.data.interfaces.product_repository_interface import ProductRepositoryInterface
 from catalog_microservice.domain.models.products import Products
 
+
+from catalog_microservice.errors.types.http_bad_request import HttpBadRequestError
+from catalog_microservice.errors.types.http_not_found import HttpNotFoundError
+
+
 class ProductFinder(ProductFinderInterface):
     def __init__(self, product_repository: ProductRepositoryInterface):
         self.product_repository = product_repository
@@ -15,16 +20,18 @@ class ProductFinder(ProductFinderInterface):
         return self.__format_response(products)
     
     @classmethod
-    def __validate_product_id(cls, product_id: int) -> Exception:
+    def __validate_product_id(cls, product_id: int) -> None:
         if not product_id or type(product_id) != int:
-            raise Exception("Product ID must be a number")
+            raise HttpBadRequestError("Product ID must be a number")
         if product_id < 0:
-            raise Exception("Product ID must be a positive number")
+            raise HttpBadRequestError("Product ID must be a positive number")
         
     def __search_product(self, product_id: int) -> List[Products]:
+        
         products = self.product_repository.select_product(product_id)                  
-        if products == []:
-            raise Exception("Product not found")
+        print (products)
+        if products == []: raise HttpNotFoundError("Product not found")
+        
         return products
         
         
--- catalog_microservice/data/use_cases/product_register.py
+++ catalog_microservice/data/use_cases/product_register.py
@@ -4,6 +4,8 @@ from catalog_microservice.domain.use_cases.product_register_interface import Pro
 from catalog_microservice.data.interfaces.product_repository_interface import ProductRepositoryInterface
 from catalog_microservice.domain.models.products import Products
 
+from catalog_microservice.errors.types.http_unprocessable_content import HttpUnprocessableContentError
+
 class ProductRegister(ProductRegisterInterface):
     def __init__(self, product_repository: ProductRepositoryInterface):
         self.product_repository = product_repository
@@ -19,13 +21,13 @@ class ProductRegister(ProductRegisterInterface):
     @classmethod
     def __validate_product(cls, name: str, description: str, price: float, category_id: int) -> Exception:
         if not name or type(name) != str:
-            raise Exception("Name must be a string")
+            raise HttpUnprocessableContentError("Name must be a string")
         if not description or type(description) != str:
-            raise Exception("Description must be a string")
+            raise HttpUnprocessableContentError("Description must be a string")
         if not price or type(price) != float:
-            raise Exception("Price must be a float")
+            raise HttpUnprocessableContentError("Price must be a float")
         if not category_id or type(category_id) != int:
-            raise Exception("Category_id must be a integer")
+            raise HttpUnprocessableContentError("Category_id must be a integer")
         
     def __create_product(self, name: str, description: str, price: float, category_id: int) -> Products:
         self.product_repository.insert_product(
