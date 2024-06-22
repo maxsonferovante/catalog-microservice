b80868884d86c3766c29c6c000123aaab8c23193
Maxson Almeida <maxsonferovante@gmail.com>
2024-06-22 18:07:28 -0300

    refactor: Update database entity classes

--- catalog_microservice/infra/database/entities/categorys.py
+++ catalog_microservice/infra/database/entities/categorys.py
@@ -1,15 +1,18 @@
 from datetime import datetime
 
 from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import mapped_column, Mapped
 from catalog_microservice.infra.database.settings.base import Base
 
 class Categorys(Base):
     __tablename__ = 'categories'
-    id = Column(Integer, primary_key=True,autoincrement=True, index=True)
-    name = Column(String, index=True)
-    description = Column(String)
+    id: Mapped[int] = mapped_column(primary_key=True, type_=Integer, autoincrement=True)
+    
+    name = Column(String(255))
+    description = Column(String(255))
     created_at = Column(DateTime, autoincrement=True, default=datetime.now)
     updated_at = Column(DateTime, autoincrement=True, default=datetime.now, onupdate=datetime.now)
     
     def __repr__(self):
         return f'Category(id={self.id}, name={self.name}, description={self.description})'
+    
--- catalog_microservice/infra/database/entities/products.py
+++ catalog_microservice/infra/database/entities/products.py
@@ -1,21 +1,24 @@
 from datetime import datetime
 
 from sqlalchemy import Column, Integer, DateTime, Numeric, String
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import mapped_column, Mapped, relationship
 
 from catalog_microservice.infra.database.settings.base import Base
-from sqlalchemy.sql.schema import ForeignKey
-from sqlalchemy.orm import relationship
 from catalog_microservice.infra.database.entities.categorys import Categorys
 
 
 class Products(Base):
     __tablename__ = 'products'
 
-    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
-    name = Column(String, index=True)
-    description = Column(String)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    
+    name = Column(String(255))
+    description = Column(String(255))
     price = Column(Numeric(10, 2))
-    category_id = Column(Integer, ForeignKey('categories.id'))
+    
+    category_id: Mapped[int] = mapped_column(ForeignKey('categories.id'))
+    
     created_at = Column(DateTime, autoincrement=True, default=datetime.now)
     updated_at = Column(DateTime, autoincrement=True, default=datetime.now, onupdate=datetime.now)
     
