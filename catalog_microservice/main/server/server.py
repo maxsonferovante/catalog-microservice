5afdc6dccb8dd2721a779db7c190906fb99e14b1
Maxson Almeida <maxsonferovante@gmail.com>
2024-06-25 00:21:30 -0300

    refactor: Register category routes in server.py

--- catalog_microservice/main/server/server.py
+++ catalog_microservice/main/server/server.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 
 from catalog_microservice.main.routes.product_routes import product_route_bp
+from catalog_microservice.main.routes.categery_routes import category_route_bp
 from catalog_microservice.main.routes.routes import home_bp
 
 app = Flask(__name__)
@@ -12,3 +13,5 @@ CORS(app)
 
 app.register_blueprint(home_bp)
 app.register_blueprint(product_route_bp)
+app.register_blueprint(category_route_bp)
+
