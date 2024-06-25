222a0ad91e372961db1da93f87fbe3e8ec12c11c
Maxson Almeida <maxsonferovante@gmail.com>
2024-06-25 00:06:41 -0300

    feat: Add UUID validation for category_id in product_register_validator

--- catalog_microservice/validators/product_finder_validator.py
+++ catalog_microservice/validators/product_finder_validator.py
@@ -1,25 +1,26 @@
 from cerberus import Validator
-
-
 from catalog_microservice.errors.types.http_unprocessable_content import HttpUnprocessableContentError
+
+
+
 def product_finder_validator(request: any):
-    
+    regex_uuid = '^[a-f0-9]{8}-[a-f0-9]{4}-5[a-f0-9]{3}-[89ab][a-f0-9]{3}-[a-f0-9]{12}$'
+        
     document_query = Validator({
             'product_id': {
                 'type': 'string', 
                 'required': True, 
                 'empty': False, 
-                'regex':  '^[1-9]+$',
+                'regex': regex_uuid,
                  }
             })
         
     response = document_query.validate(request.args)
     if response is False:
-        # {'product_id': ["value does not match regex '^\\d+$'"]}
         for key, value in document_query.errors.items():
             print (key, value)
-            if value[0] == "value does not match regex '^[1-9]+$'":           
-                raise HttpUnprocessableContentError({key: 'The product_id must be an integer'})    
+            if value[0] == "value does not match regex '{}'".format(regex_uuid):           
+                raise HttpUnprocessableContentError({key: 'The product_id must be an UUID string'}) 
         raise HttpUnprocessableContentError(document_query.errors)
     
 
--- catalog_microservice/validators/product_register_validator.py
+++ catalog_microservice/validators/product_register_validator.py
@@ -3,17 +3,20 @@ from cerberus import Validator
 from catalog_microservice.errors.types.http_unprocessable_content import HttpUnprocessableContentError
 
 def product_register_validator(request: any):
-    
+    regex_uuid = '^[a-f0-9]{8}-[a-f0-9]{4}-5[a-f0-9]{3}-[89ab][a-f0-9]{3}-[a-f0-9]{12}$'
     document_validator = Validator(
         {
          'name': {'type': 'string', 'required': True, 'empty': False, 'minlength': 3, 'maxlength': 50},
          'description': {'type': 'string', 'required': True, 'empty': False, 'minlength': 3, 'maxlength': 255},
          'price': {'type': 'float', 'required': True, 'empty': False, 'min': 0.0},
-         'category_id': {'type': 'integer', 'required': True, 'empty': False, 'min': 1}
+         'category_id': {'type': 'string', 'required': True, 'empty': False, 'regex': regex_uuid}
         })
     
     
     response = document_validator.validate(request.json)
     if response is False:
-            raise HttpUnprocessableContentError(document_validator.errors)
+        for key, value in document_validator.errors.items():
+            if value[0] == "value does not match regex '{}'".format(regex_uuid):           
+                raise HttpUnprocessableContentError({key: 'The category_id must be an UUID string'})
+        raise HttpUnprocessableContentError(document_validator.errors)
     
