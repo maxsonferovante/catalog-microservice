82b0b02eae0c3a87574f6efa90bb4022137d3ff8
Maxson Almeida <maxsonferovante@gmail.com>
2024-06-26 17:26:50 -0300

    refactor: Update main.py to use FlaskApp class and load environment variables

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-from catalog_microservice.run import run
+from dotenv import load_dotenv
+import os
 
-
+from catalog_microservice.app import FlaskApp
+from catalog_microservice.main.server.server import app
 
 if __name__ == '__main__':
-    run()
+    load_dotenv()        
+    options = {
+        'bind': os.environ['BIND_HOST_PORT'],
+        'workers': os.environ['NUM_WORKERS'],
+        'timeout': os.environ['TIMEOUT'],
+    }
+    print (options)
+    FlaskApp(
+        app=app,
+        options=options
+    ).run()
