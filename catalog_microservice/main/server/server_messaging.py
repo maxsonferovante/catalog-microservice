d6c35becf96966754c442c69b7a6dcd867e880f9
Maxson Almeida <maxsonferovante@gmail.com>
2024-07-02 23:41:06 -0300

    refactor: Add consumer for product stock update messaging

--- catalog_microservice/main/server/server_messaging.py
+++ catalog_microservice/main/server/server_messaging.py
@@ -1,15 +1,31 @@
+from multiprocessing import Process
 from catalog_microservice.main.routes.messaging.product_message_routes import consumer_stock_update
 
 class ServerMessaging:
         
         def __init__(self) -> None:
-            pass
-        
-        def start_message_consumers(self):
-            consumer_stock_update()
+            self.list_consumer = []
+            self.list_process = []
+        def add_consumer(self, consumer):
+            self.list_consumer.append(consumer)
             
-        def start_message_producers(self):
-            pass    
+        def run(self):
+            self.list_process = [Process(target=consumer_stock_update, name= consumer.__name__) 
+                                 for consumer in self.list_consumer]
+            for process in self.list_process:
+                print ("Starting process {}".format(process.name))
+                process.start()
+                print ("Process {} started".format(process.name))
+
+        def stop(self):
+            for process in self.list_process:
+                process.terminate()
+                process.join()
+                print ("Process {} stopped".format(process.name))
+            
+        
 
 
 
+server_messaging = ServerMessaging()
+server_messaging.add_consumer(consumer=consumer_stock_update)
